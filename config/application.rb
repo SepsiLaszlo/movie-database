4a1921b3c5843764875c987ace441a169489292c
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2023-09-04 16:15:53 +0200

    add deployment config

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+CACHE_DURATION = 2.minutes
+
 module MovieDatabase
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.

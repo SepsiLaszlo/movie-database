b01f6b4c097b537dc988bc604d469c390b6603f7
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2023-09-04 16:32:21 +0200

    add views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def pagination_page_numbers(current_page:, total_pages:)
+    pagination_pages = []
+    nearby_pages = ((current_page - 4)..(current_page + 4)).sort_by { |page| (current_page - page).abs }
+    nearby_pages.each do |page|
+      pagination_pages << page if 0 < page && page <= total_pages && pagination_pages.length < 5
+    end
+    pagination_pages.sort
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener('turbo:load',event => {
+    if(screen.width < 1280) return; // disable for mobile
+    const input = document.getElementById("query")
+    input.focus()
+    const val = input.value;
+    input.value = '';
+    input.value = val;
+})
+
